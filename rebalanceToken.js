47d571149bf5ee618975b16b726ecb708288314a
Shibu Gope <shibug@gmail.com>
2023-06-05 18:56:48 -0500

    changes

--- rebalanceToken.js
+++ rebalanceToken.js
@@ -1,10 +1,11 @@
 //Rebalances excess DATA from staked nodes and moves to another wallet
 
 const Web3 = require("web3");
-const NETWORK = "polygon-mainnet"
-const INFURA_API_KEY = "e688007f8726451192c518e37fe0cdda"
+const NETWORK = "polygon-mainnet";
+const INFURA_API_KEY = "e688007f8726451192c518e37fe0cdda";
 const tokenAddress = "0x3a9A81d576d83FF21f26f325066054540720fC34";
-let minABI = [
+const minABI = [
+  // ABI for balanceOf function
   {
     "constant": true,
     "inputs": [
@@ -24,6 +25,7 @@ let minABI = [
     "stateMutability": "view",
     "type": "function"
   },
+  // ABI for transfer function
   {
     "constant": false,
     "inputs": [
@@ -56,10 +58,9 @@ const web3 = new Web3(
 // Create a new instance of the token contract
 const contract = new web3.eth.Contract(minABI, tokenAddress);
 
+// Function to get the token balance of a wallet address
 async function getTokenBalance(walletAddress, tokenAddress) {
-
   try {
-    
     // Get the balance of the token in the wallet address
     const balance = await contract.methods.balanceOf(walletAddress).call();
 
@@ -73,49 +74,49 @@ async function getTokenBalance(walletAddress, tokenAddress) {
   }
 }
 
+// Function to send tokens from a wallet
 async function send(signerPrivateKey, destinationAddress, amount) {
+  try {
+    const value = web3.utils.toWei(String(amount));
 
-  let value = web3.utils.toWei(String(amount));
+    // Call transfer function and encode the parameters
+    const data = contract.methods.transfer(destinationAddress, value).encodeABI();
 
-  // call transfer function
-  let data = contract.methods.transfer(destinationAddress, value).encodeABI();
+    // Create a signing account from a private key
+    const signer = web3.eth.accounts.privateKeyToAccount(signerPrivateKey);
+    web3.eth.accounts.wallet.add(signer);
 
-  // Creating a signing account from a private key
-  const signer = web3.eth.accounts.privateKeyToAccount(signerPrivateKey);
-  web3.eth.accounts.wallet.add(signer);
-  
-  // Creating the transaction object
-  const tx = {
-    from: signer.address,
-    to: tokenAddress,
-    data: data,
-    gasPrice: web3.utils.toWei('160', 'gwei'),
-    gasLimit: 1000000
-  };
+    // Create the transaction object
+    const tx = {
+      from: signer.address,
+      to: tokenAddress,
+      data: data,
+      gasPrice: web3.utils.toWei('160', 'gwei'),
+      gasLimit: 1000000
+    };
 
-  // Estimate gas
-  tx.gas = await web3.eth.estimateGas(tx);
+    // Estimate gas
+    tx.gas = await web3.eth.estimateGas(tx);
 
-  // Sending the transaction to the network
-  const receipt = await web3.eth
-    .sendTransaction(tx)
-    .once("transactionHash", (txhash) => {
+    // Send the transaction to the network
+    const receipt = await web3.eth.sendTransaction(tx).once("transactionHash", (txhash) => {
       console.log(`Transaction hash: ${txhash}`);
     });
+  } catch (error) {
+    console.error('Error sending funds:', error);
+    throw error;
+  }
 }
 
 getTokenBalance(process.argv[2], tokenAddress)
   .then(balance => {
     console.log(`Token balance: ${balance} DATA in wallet: ${process.argv[2]}`);
     if (balance > 20000) {
-
       const privateKey = process.argv[3];
       const destinationAddress = process.argv[4];
       const amountToSend = balance - 20000;
       if (amountToSend > 10) {
-        
         console.log(`Sending ${amountToSend} DATA to ${destinationAddress}`);
-        
         send(privateKey, destinationAddress, amountToSend)
           .then(() => {
             console.log('Funds sent successfully!');
@@ -129,7 +130,7 @@ getTokenBalance(process.argv[2], tokenAddress)
     } else {
       console.log('Token balance is at or below 20,000 DATA. No funds sent.');
     }
-})
-.catch(error => {
-  console.error('Error retrieving token balance from wallet:', error);
-});
+  })
+  .catch(error => {
+    console.error('Error retrieving token balance from wallet:', error);
+  });
