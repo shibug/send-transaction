e0897e085301422ca2a3f02effe82e3ef9f0fb33
Shibu Gope <shibug@gmail.com>
2023-06-05 19:05:32 -0500

    changes

--- rebalanceToken.js
+++ rebalanceToken.js
@@ -7,59 +7,58 @@ const tokenAddress = "0x3a9A81d576d83FF21f26f325066054540720fC34";
 const minABI = [
   // ABI for balanceOf function
   {
-    "constant": true,
-    "inputs": [
+    constant: true,
+    inputs: [
       {
-        "name": "_owner",
-        "type": "address"
+        name: "_owner",
+        type: "address"
       }
     ],
-    "name": "balanceOf",
-    "outputs": [
+    name: "balanceOf",
+    outputs: [
       {
-        "name": "balance",
-        "type": "uint256"
+        name: "balance",
+        type: "uint256"
       }
     ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
+    payable: false,
+    stateMutability: "view",
+    type: "function"
   },
   // ABI for transfer function
   {
-    "constant": false,
-    "inputs": [
+    constant: false,
+    inputs: [
       {
-        "name": "_to",
-        "type": "address"
+        name: "_to",
+        type: "address"
       },
       {
-        "name": "_value",
-        "type": "uint256"
+        name: "_value",
+        type: "uint256"
       }
     ],
-    "name": "transfer",
-    "outputs": [
+    name: "transfer",
+    outputs: [
       {
-        "name": "",
-        "type": "bool"
+        name: "",
+        type: "bool"
       }
     ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
   }
 ];
 
 // Initialize a connection to the Polygon (Matic) network
-const web3 = new Web3(
-  new Web3.providers.HttpProvider(`https://${NETWORK}.infura.io/v3/${INFURA_API_KEY}`)
-);
+const web3 = new Web3(`https://${NETWORK}.infura.io/v3/${INFURA_API_KEY}`);
+
 // Create a new instance of the token contract
 const contract = new web3.eth.Contract(minABI, tokenAddress);
 
 // Function to get the token balance of a wallet address
-async function getTokenBalance(walletAddress, tokenAddress) {
+async function getTokenBalance(walletAddress) {
   try {
     // Get the balance of the token in the wallet address
     const balance = await contract.methods.balanceOf(walletAddress).call();
@@ -77,7 +76,7 @@ async function getTokenBalance(walletAddress, tokenAddress) {
 // Function to send tokens from a wallet
 async function send(signerPrivateKey, destinationAddress, amount) {
   try {
-    const value = web3.utils.toWei(String(amount));
+    const value = web3.utils.toWei(amount.toString());
 
     // Call transfer function and encode the parameters
     const data = contract.methods.transfer(destinationAddress, value).encodeABI();
@@ -96,41 +95,42 @@ async function send(signerPrivateKey, destinationAddress, amount) {
     };
 
     // Estimate gas
-    tx.gas = await web3.eth.estimateGas(tx);
+    const gas = await web3.eth.estimateGas(tx);
+    tx.gas = gas;
 
     // Send the transaction to the network
-    const receipt = await web3.eth.sendTransaction(tx).once("transactionHash", (txhash) => {
-      console.log(`Transaction hash: ${txhash}`);
-    });
+    const receipt = await web3.eth.sendTransaction(tx);
+    console.log(`Transaction hash: ${receipt.transactionHash}`);
   } catch (error) {
     console.error('Error sending funds:', error);
     throw error;
   }
 }
 
-getTokenBalance(process.argv[2], tokenAddress)
-  .then(balance => {
-    console.log(`Token balance: ${balance} DATA in wallet: ${process.argv[2]}`);
+async function main() {
+  try {
+    const walletAddress = process.argv[2];
+    const privateKey = process.argv[3];
+    const destinationAddress = process.argv[4];
+
+    const balance = await getTokenBalance(walletAddress);
+    console.log(`Token balance: ${balance} DATA in wallet: ${walletAddress}`);
+
     if (balance > 20000) {
-      const privateKey = process.argv[3];
-      const destinationAddress = process.argv[4];
       const amountToSend = balance - 20000;
       if (amountToSend > 10) {
         console.log(`Sending ${amountToSend} DATA to ${destinationAddress}`);
-        send(privateKey, destinationAddress, amountToSend)
-          .then(() => {
-            console.log('Funds sent successfully!');
-          })
-          .catch(error => {
-            console.error('Error sending funds:', error);
-          });
+        await send(privateKey, destinationAddress, amountToSend);
+        console.log('Funds sent successfully!');
       } else {
         console.log('Excess token balance is below 10 DATA. No funds sent.');
       }
     } else {
       console.log('Token balance is at or below 20,000 DATA. No funds sent.');
     }
-  })
-  .catch(error => {
-    console.error('Error retrieving token balance from wallet:', error);
-  });
+  } catch (error) {
+    console.error('Error retrieving token balance or sending funds:', error);
+  }
+}
+
+main();
