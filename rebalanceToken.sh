2d61b7f95913e99179d5f0a489d7bc2324407f4c
Shibu Gope <shibug@gmail.com>
2023-08-12 14:25:46 -0500

    changes

--- rebalanceToken.sh
+++ rebalanceToken.sh
@@ -5,6 +5,6 @@ input_file="wallets.txt"
 # Read the file line by line
 while IFS=$'\t' read -r address privateKey; do
   echo
-  node rebalanceToken.js ${address} ${privateKey} 0x56a5f2aad2bdc06278c69f43b0a10c5558506338
+  node rebalanceToken.js ${address} ${privateKey} 0x7925f4dc61843d2b1008138cef88812f24bfbded 
 
 done < "$input_file"
